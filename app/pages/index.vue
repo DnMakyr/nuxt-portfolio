95983ae0664c23872209f21a9b205bc7afa354ab
DnMakyr <dnsc1503@gmail.com>
2024-10-24 02:57:32 +0700

    chore: add SEO

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,9 +5,32 @@
 </template>
 
 <script lang="ts" setup>
+useSeoMeta({
+  title: 'Main',
+  description: 'This is the main page',
+  ogTitle: 'Main',
+  ogDescription: 'This is the main page',
+  ogImage: '[og:image]',
+  ogUrl: 'og:url',
+  twitterTitle: '[twitter:title]',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
 
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
 </script>
 
 <style>
-
 </style>
